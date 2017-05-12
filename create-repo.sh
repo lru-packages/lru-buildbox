296ff738d80c226d4af624bbee2e97daee22c85c
Ryan Parman <ryan@ryanparman.com>
2017-05-12 09:25:24 -0700

    Update create-repo.sh

--- create-repo.sh
+++ create-repo.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
+OLD=$(pwd)
 for directory in $(find $(pwd)/repo -type d | grep -i repodata | xargs dirname); do
 	echo "===> $directory";
 	cd $directory;
 	createrepo .
 	echo " ";
-	cd /vagrant;
+	cd $OLD;
 done;
